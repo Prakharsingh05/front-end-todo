8ada6588a04e15b8d01882f6d9b040dab7099b21
Prakhar Singh <prakharsingh285@gmail.com>
2023-11-15 10:32:42 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,92 +1,87 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
-import {AiOutlineDelete} from 'react-icons/ai';
-import {BsCheckLg} from 'react-icons/bs';
-function App () {
-  const [allTodos, setAllTodos] = useState ([]);
-  const [newTodoTitle, setNewTodoTitle] = useState ('');
-  const [newDescription, setNewDescription] = useState ('');
-  const [completedTodos, setCompletedTodos] = useState ([]);
-  const [isCompletedScreen, setIsCompletedScreen] = useState (false);
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { AiOutlineDelete } from "react-icons/ai";
+import { BsCheckLg } from "react-icons/bs";
+function App() {
+  const [allTodos, setAllTodos] = useState([]);
+  const [newTodoTitle, setNewTodoTitle] = useState("");
+  const [newDescription, setNewDescription] = useState("");
+  const [completedTodos, setCompletedTodos] = useState([]);
+  const [isCompletedScreen, setIsCompletedScreen] = useState(false);
 
   const handleAddNewToDo = () => {
     let newToDoObj = {
       title: newTodoTitle,
       description: newDescription,
     };
-    
 
     let updatedTodoArr = [...allTodos];
-    updatedTodoArr.push (newToDoObj);
-   
+    updatedTodoArr.push(newToDoObj);
 
-    setAllTodos (updatedTodoArr);
-    localStorage.setItem ('todolist', JSON.stringify (updatedTodoArr));
-    setNewDescription ('');
-    setNewTodoTitle ('');
+    setAllTodos(updatedTodoArr);
+    localStorage.setItem("todolist", JSON.stringify(updatedTodoArr));
+    setNewDescription("");
+    setNewTodoTitle("");
   };
 
-  useEffect (() => {
-    let savedTodos = JSON.parse (localStorage.getItem ('todolist'));
-    let savedCompletedToDos = JSON.parse (
-      localStorage.getItem ('completedTodos')
+  useEffect(() => {
+    let savedTodos = JSON.parse(localStorage.getItem("todolist"));
+    let savedCompletedToDos = JSON.parse(
+      localStorage.getItem("completedTodos")
     );
     if (savedTodos) {
-      setAllTodos (savedTodos);
+      setAllTodos(savedTodos);
     }
 
     if (savedCompletedToDos) {
-      setCompletedTodos (savedCompletedToDos);
+      setCompletedTodos(savedCompletedToDos);
     }
   }, []);
 
-  const handleToDoDelete = index => {
+  const handleToDoDelete = (index) => {
     let reducedTodos = [...allTodos];
-    reducedTodos.splice (index,1);
-    
-    localStorage.setItem ('todolist', JSON.stringify (reducedTodos));
-    setAllTodos (reducedTodos);
+    reducedTodos.splice(index, 1);
+
+    localStorage.setItem("todolist", JSON.stringify(reducedTodos));
+    setAllTodos(reducedTodos);
   };
 
-  const handleCompletedTodoDelete = index => {
+  const handleCompletedTodoDelete = (index) => {
     let reducedCompletedTodos = [...completedTodos];
-    reducedCompletedTodos.splice (index);
-    
-    localStorage.setItem (
-      'completedTodos',
-      JSON.stringify (reducedCompletedTodos)
+    reducedCompletedTodos.splice(index);
+
+    localStorage.setItem(
+      "completedTodos",
+      JSON.stringify(reducedCompletedTodos)
     );
-    setCompletedTodos (reducedCompletedTodos);
+    setCompletedTodos(reducedCompletedTodos);
   };
 
-  const handleComplete = index => {
-    const date = new Date ();
-    var dd = date.getDate ();
-    var mm = date.getMonth () + 1;
-    var yyyy = date.getFullYear ();
-    var hh = date.getHours ();
-    var minutes = date.getMinutes ();
-    var ss = date.getSeconds ();
+  const handleComplete = (index) => {
+    const date = new Date();
+    var dd = date.getDate();
+    var mm = date.getMonth() + 1;
+    var yyyy = date.getFullYear();
+    var hh = date.getHours();
+    var minutes = date.getMinutes();
+    var ss = date.getSeconds();
     var finalDate =
-      dd + '-' + mm + '-' + yyyy + ' at ' + hh + ':' + minutes + ':' + ss;
+      dd + "-" + mm + "-" + yyyy + " at " + hh + ":" + minutes + ":" + ss;
 
     let filteredTodo = {
       ...allTodos[index],
       completedOn: finalDate,
     };
 
-   
-
     let updatedCompletedList = [...completedTodos, filteredTodo];
-    console.log (updatedCompletedList);
-    setCompletedTodos (updatedCompletedList);
-    localStorage.setItem (
-      'completedTodos',
-      JSON.stringify (updatedCompletedList)
+    console.log(updatedCompletedList);
+    setCompletedTodos(updatedCompletedList);
+    localStorage.setItem(
+      "completedTodos",
+      JSON.stringify(updatedCompletedList)
     );
-    
 
-    handleToDoDelete (index);
+    handleToDoDelete(index);
   };
 
   return (
@@ -94,14 +89,13 @@ function App () {
       <h1>Todo List</h1>
 
       <div className="todo-wrapper">
-
         <div className="todo-input">
           <div className="todo-input-item">
             <label>Title:</label>
             <input
               type="text"
               value={newTodoTitle}
-              onChange={e => setNewTodoTitle (e.target.value)}
+              onChange={(e) => setNewTodoTitle(e.target.value)}
               placeholder="What's the title of your To Do?"
             />
           </div>
@@ -110,7 +104,7 @@ function App () {
             <input
               type="text"
               value={newDescription}
-              onChange={e => setNewDescription (e.target.value)}
+              onChange={(e) => setNewDescription(e.target.value)}
               placeholder="What's the description of your To Do?"
             />
           </div>
@@ -126,55 +120,58 @@ function App () {
         </div>
         <div className="btn-area">
           <button
-            className={`secondaryBtn ${isCompletedScreen === false && 'active'}`}
-            onClick={() => setIsCompletedScreen (false)}
+            className={`secondaryBtn ${
+              isCompletedScreen === false && "active"
+            }`}
+            onClick={() => setIsCompletedScreen(false)}
           >
-            Incomplete
+            Pending
           </button>
           <button
-            className={`secondaryBtn ${isCompletedScreen === true && 'active'}`}
-            onClick={() => setIsCompletedScreen (true)}
+            className={`secondaryBtn ${isCompletedScreen === true && "active"}`}
+            onClick={() => setIsCompletedScreen(true)}
           >
-            Completed
+            Done
           </button>
         </div>
         <div className="todo-list">
-
           {isCompletedScreen === false &&
-            allTodos.map ((item, index) => (
+            allTodos.map((item, index) => (
               <div className="todo-list-item" key={index}>
                 <div>
                   <h3>{item.title}</h3>
                   <p>{item.description}</p>
-
                 </div>
                 <div>
                   <AiOutlineDelete
                     title="Delete?"
                     className="icon"
-                    onClick={() => handleToDoDelete (index)}
+                    onClick={() => handleToDoDelete(index)}
                   />
                   <BsCheckLg
                     title="Completed?"
                     className=" check-icon"
-                    onClick={() => handleComplete (index)}
+                    onClick={() => handleComplete(index)}
                   />
                 </div>
               </div>
             ))}
 
           {isCompletedScreen === true &&
-            completedTodos.map ((item, index) => (
+            completedTodos.map((item, index) => (
               <div className="todo-list-item" key={index}>
                 <div>
                   <h3>{item.title}</h3>
                   <p>{item.description}</p>
-                  <p> <i>Completed at: {item.completedOn}</i></p>
+                  <p>
+                    {" "}
+                    <i>Completed at: {item.completedOn}</i>
+                  </p>
                 </div>
                 <div>
                   <AiOutlineDelete
                     className="icon"
-                    onClick={() => handleCompletedTodoDelete (index)}
+                    onClick={() => handleCompletedTodoDelete(index)}
                   />
                 </div>
               </div>
